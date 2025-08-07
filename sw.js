2b1320e990e629dcdde86dca50f5ef8e012274a0
Kunal Damania <kunaldamania45@gmail.com>
2025-08-07 23:24:22 +0530

    Feat: Add PWA capabilities

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = 'multiplier-wheel-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json'
+  // Note: Add paths to your main CSS and JS files if they are separate
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response; // Serve from cache
+        }
+        return fetch(event.request); // Fetch from network
+      })
+  );
+});
