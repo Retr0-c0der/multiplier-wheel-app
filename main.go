efd3de200d8f3b39143eed5a534905e6f3937927
Kunal Damania <kunaldamania45@gmail.com>
2025-08-07 21:54:34 +0530

    Initial commit

---
+++ main.go
@@ -0,0 +1,64 @@
+// File: main.go
+package main
+
+import (
+	"context"
+	"embed"
+
+	"github.com/wailsapp/wails/v2"
+	"github.com/wailsapp/wails/v2/pkg/options"
+	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
+	"github.com/wailsapp/wails/v2/pkg/runtime"
+)
+
+//go:embed all:frontend/dist
+var assets embed.FS
+
+// App struct
+type App struct {
+	ctx context.Context
+}
+
+// NewApp creates a new App application struct
+func NewApp() *App {
+	return &App{}
+}
+
+// startup is called when the app starts. The context is saved
+// so we can call the runtime methods
+func (a *App) startup(ctx context.Context) {
+	a.ctx = ctx
+}
+
+// Shutdown is a method that can be called from the frontend to close the app.
+func (a *App) Shutdown() {
+	runtime.Quit(a.ctx)
+}
+
+// This is the main entry point for the application
+func main() {
+	// Create an instance of the app structure
+	app := NewApp()
+
+	// Create application with options
+	err := wails.Run(&options.App{
+		Title:  "lottery-app",
+		Width:  550, // Adjusted width
+		Height: 600, // Adjusted height
+		AssetServer: &assetserver.Options{
+			Assets: assets,
+		},
+		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
+		OnStartup:        app.startup,
+		Bind: []interface{}{
+			app,
+		},
+		Frameless:     true, // Make window frameless
+		AlwaysOnTop:   true, // Make window always on top
+		DisableResize: true, // Disable resizing
+	})
+
+	if err != nil {
+		println("Error:", err.Error())
+	}
+}
